bd751399600f874f42d41bf400c3966e88d54b72
rg <rg@slowchat.us>
2015-07-09 20:45:29 -0700

    completed :Adding a Slot Struct

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by gambord on 7/9/15.
+//  Copyright (c) 2015 bitfountain-student. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+  var value = 0
+  var image = UIImage(named: "ace")
+  var isRed = true
+}
